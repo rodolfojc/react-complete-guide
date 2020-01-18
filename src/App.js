d31b8958e7e78498e917f678fa70b921ca2bbb45
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-18 09:27:13 +0000

    Props and State

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import Person from './Person/Person';
 
 // Class based Component 
 class App extends Component {
+
+  // Defining properties in Class Based Conponents - Extends Components
+  // Reserved Word - STATE
+  state = {
+    persons: [
+      { name: 'Rodolfo', age: 32 },
+      { name: 'Juan', age: 45 },
+      { name: 'Cesar', age: 21 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Rodolfo</h1>
         <p>This is a test</p>
-        <Person name="Rodolfo" age="32" />
-        <Person name="Juan" age="45">I'm Venezuelan</Person>
-        <Person name="Cesar" age="21" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm Venezuelan</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
 
