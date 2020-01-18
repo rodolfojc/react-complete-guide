a4c75af9c543e5cb8fa1b3c3d267b950ecc769e8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-18 09:18:30 +0000

    Props - Children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import React from 'react';
 // Functional Component 
 const person = (props) => {
 
-    return <p>I'm {props.name} and I am {props.age} years old!!</p>
-
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!!</p>
+            <p>{props.children}</p>
+        </div>
+    
+    )
     // Using JS Functions
     //return <p>I'm a Person and I am {Math.floor(Math.random()*30)} years old!!</p>
 };
