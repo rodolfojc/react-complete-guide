fde6d8a3b50f666533dc5eac1e91f7477387778e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-18 09:05:11 +0000

    Functional and Class Components  - JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
+// Class based Component 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Rodolfo</h1>
+        <p>This is a test</p>
+        <Person />
       </div>
     );
+
+  // JSX => DEMO 
+  //return React.createElement('div', { className: 'App'} , React.createElement('h1', null, 'Hola!!'), 'Hello There!');
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Functional Component 
+const person = () => {
+    return <p>I'm a Person!</p>
+};
+
+export default person;
