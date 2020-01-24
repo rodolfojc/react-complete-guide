b3e37556d29e3e352531023538b47f810daa559e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-24 10:28:21 +0000

    CSS Modules and Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,44 +1,17 @@
 import React from 'react';
-import './Person.css';
-//import Radium, { StyleRoot } from 'radium';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-@media (min-width: 500px) {    
-        width: 450px;    
-}
-
-`;
-
+import classes from './Person.css';
 
 // Functional Component 
 const person = (props) => {
 
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
-
     return (
-        //<div className="Person" style={style}>
-       <StyledDiv>
+        <div className={classes.Person}>     
             <p onClick= {props.click}>I'm {props.name} and I am {props.age} years old!!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value = {props.name}/>
-        </StyledDiv>
-    
+        </div> 
     )
-    // Using JS Functions
-    //return <p>I'm a Person and I am {Math.floor(Math.random()*30)} years old!!</p>
+    
 };
 
 export default person;
