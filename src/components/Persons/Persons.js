7fd5cd486b17830117991bdf0bbd30b53c4cf0cc
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-25 10:53:41 +0000

    Component Update Lifecycle (for props Changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
+// Converting Persons.js Functional to Class-based
+class Persons extends Component {
+
+  // Lifecicles
+
+  // COMMENTED BECAUSE CREATES AND ERROR AS STATE IS NOT NECESSARY TO UPDATE
+  // static getDerivedStateFromProps (props, state) {
+  //   console.log('[Persons.js] - getDerivedStateFromProps()');
+  //   return state;
+  // };
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('[Persons.js] - shouldComponentUpdate()');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate (prevProps, prevState) {
+    console.log('[Persons.js] - getSnapshotBeforeUpdate()');
+    return { message: 'Snapshot!'};    
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    console.log('[Persons.js] - componentDidUpdate()');
+    console.log(snapshot);
+  }
+
+  render () {
   console.log('[Persons.js] - Rendering...');
-  return props.persons.map((person, index) =>{
+  return this.props.persons.map((person, index) =>{
        return (<Person
-          click={() => props.clicked(index)} 
+          click={() => this.props.clicked(index)} 
           name={person.name} 
           age={person.age}             
           key={person.id}
-          changed={(event) => props.changed(event, person.id)}
+          changed={(event) => this.props.changed(event, person.id)}
               />
     );
   })
 };
+}
 
-export default persons;
+export default Persons;
