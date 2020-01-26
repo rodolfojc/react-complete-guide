3d8ef98f3a374952f643d48be411463e4ca4d6c8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-01-26 16:44:14 +0000

    Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import classes from "./Person.css";
 import Aux from "../../../hoc/Auxilliary";
 
@@ -7,7 +7,7 @@ class Person extends Component {
   render() {
     console.log("[Person.js] - Rendering");
     return (
-      <Aux>
+      <Fragment>
         <p key="i1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!!
         </p>
@@ -18,7 +18,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </Aux>
+      </Fragment>
     );
   }
 }
